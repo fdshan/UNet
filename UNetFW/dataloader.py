f4145ab4b039891267b6cfa04cb67517d0db814f
fdshan <74119964+fdshan@users.noreply.github.com>
2021-01-31 22:50:23 -0400

    Add files via upload

--- UNetFW/dataloader.py
+++ UNetFW/dataloader.py
@@ -6,6 +6,7 @@ import random
 import numpy as np
 from PIL import Image
 
+
 class DataLoader():
     def __init__(self, root_dir='data', batch_size=2, test_percent=.1):
         self.batch_size = batch_size
@@ -34,7 +35,42 @@ class DataLoader():
             current += 1
 
             # todo: load images and labels
+            curr_image = Image.open(self.data_files[current])
+            curr_label = Image.open(self.label_files[current])
+
+            # ---------------------- Data augmentation ----------------------
+            # Flip the image horizontally
+            data_image = curr_image.transpose(Image.FILP_LEFT_RIGHT)
+            label_image = curr_label.transpose(Image.FILP_LEFT_RIGHT)
+
+            # Zoom images, center of the image
+            width1, height1 = data_image.size
+            left1 = width1 / 4
+            top1 = height1 / 4
+            right1 = 3 * width1 / 4
+            bottom1 = 3 * height1 / 4
+
+            width2, height2 = label_image.size
+            left2 = width2 / 4
+            top2 = height2 / 4
+            right2 = 3 * width2 / 4
+            bottom2 = 3 * height2 / 4
+
+            data_image = data_image.crop((left1, top1, right1, bottom1))
+            data_image = data_image.resize((572, 572))
+            label_image = label_image.crop((left2, top2, right2, bottom2))
+            label_image = label_image.resize((572, 572))
+
+            # Rotate images
+            data_image = data_image.transpose(Image.ROTATE_90)
+            label_image = label_image.transpose(Image.ROTATE_90)
+
             # hint: scale images between 0 and 1
+            data_image = np.asarray(data_image)
+            data_image /= 255.0
+
+            label_image = np.asarray(label_image)
+            label_image /= 255.0
             # hint: if training takes too long or memory overflow, reduce image size!
 
             yield (data_image, label_image)
